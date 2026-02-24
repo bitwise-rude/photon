aa808643c4fc3aa12fffc21f745d36250ec83e4f
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-02-24 20:17:27 +0545

    [fixed] opcodes assignation for instructions and added mov insturctoin for D register

--- cpu.c
+++ cpu.c
@@ -16,6 +16,7 @@ static inline u16 get_pc_16(Machine* vm) {
 	// returns the simultaneous 16 bit value pointed by PC and increments the PC twice
 	u8 hi  =  vm->mem->stream[vm->PC.val ++];
 	u8 lo  =  vm->mem->stream[vm->PC.val ++];
+	printf("%x, %x, %x\n", hi,lo,combine_bytes(hi,lo));
 
 	return combine_bytes(hi,lo);
 }
@@ -37,163 +38,218 @@ static inline void reset_flag (Machine *vm, u8 flag) {
 
 void execute(Machine* vm, u8 opcode ) {
 	switch (opcode) {
+		// Decs
+		case 0xb2:
+			DPRINTF("DEC C");
+			vm->BC.lo --;
+			break;
+
 		// Movs
 		case 0x20:
 			DPRINTF("MOV A,B");
 			vm->AF.hi = vm->BC.hi;
 			break;
 
-			case 0x21:
-				DPRINTF("MOV A,C");
-				vm->AF.hi = vm->BC.lo;
-				break;
+		case 0x21:
+			DPRINTF("MOV A,C");
+			vm->AF.hi = vm->BC.lo;
+			break;
 
-			case 0x22:
-				DPRINTF("MOV A,D");
-				vm->AF.hi = vm->DE.hi;
-				break;
+		case 0x22:
+			DPRINTF("MOV A,D");
+			vm->AF.hi = vm->DE.hi;
+			break;
 
-			case 0x23:
-				DPRINTF("MOV A,E");
-				vm->AF.hi = vm->DE.lo;
-				break;
+		case 0x23:
+			DPRINTF("MOV A,E");
+			vm->AF.hi = vm->DE.lo;
+			break;
 
-			case 0x24:
-				DPRINTF("MOV A,H");
-				vm->AF.hi = vm->HL.hi;
-				break;
+		case 0x24:
+			DPRINTF("MOV A,H");
+			vm->AF.hi = vm->HL.hi;
+			break;
 
-			case 0x25:
-				DPRINTF("MOV A,L");
-				vm->AF.hi = vm->HL.lo;
-				break;
+		case 0x25:
+			DPRINTF("MOV A,L");
+			vm->AF.hi = vm->HL.lo;
+			break;
 
-			case 0x26:
-				DPRINTF("MOV A,F");
-				vm->AF.hi = vm->AF.lo;
-				break;
+		case 0x26:
+			DPRINTF("MOV A,F");
+			vm->AF.hi = vm->AF.lo;
+			break;
 
-			case 0x27:
-				DPRINTF("MOV A,M");
-				vm->AF.hi = vm->AF.lo; // TODO: IMplement
-				break;
+		case 0x27:
+			DPRINTF("MOV A,M");
+			vm->AF.hi = vm->AF.lo; // TODO: IMplement
+			break;
 
-			case 0x28:
-				DPRINTF("MOV B,A");
-				vm->BC.hi = vm->AF.hi;
-				break;
+		case 0x28:
+			DPRINTF("MOV B,A");
+			vm->BC.hi = vm->AF.hi;
+			break;
 
-			case 0x29:
-				DPRINTF("MOV B,C");
-				vm->BC.hi = vm->BC.lo;
-				break;
+		case 0x29:
+			DPRINTF("MOV B,C");
+			vm->BC.hi = vm->BC.lo;
+			break;
 
-			case 0x2A:
-				DPRINTF("MOV B,D");
-				vm->BC.hi = vm->DE.hi;
-				break;
+		case 0x2A:
+			DPRINTF("MOV B,D");
+			vm->BC.hi = vm->DE.hi;
+			break;
 
-			case 0x2B:
-				DPRINTF("MOV B,E");
-				vm->BC.hi = vm->DE.lo;
-				break;
+		case 0x2B:
+			DPRINTF("MOV B,E");
+			vm->BC.hi = vm->DE.lo;
+			break;
 
-			case 0x2C:
-				DPRINTF("MOV B,H");
-				vm->BC.hi = vm->HL.hi;
-				break;
+		case 0x2C:
+			DPRINTF("MOV B,H");
+			vm->BC.hi = vm->HL.hi;
+			break;
 
-			case 0x2D:
-				DPRINTF("MOV B, L");
-				vm->BC.hi = vm->HL.lo;
-				break;
+		case 0x2D:
+			DPRINTF("MOV B, L");
+			vm->BC.hi = vm->HL.lo;
+			break;
 
-			case 0x50:
-				DPRINTF("MOV A, im");
-				vm->AF.hi = get_pc_8(vm);
-				break;
+		case 0x2E:
+			DPRINTF("MOV B, F");
+			vm->BC.hi = vm->AF.lo;
+			break;
 
-			case 0x51:
-				DPRINTF("MOV B, im");
-				vm->BC.hi = get_pc_8(vm);
-				break;
+		case 0x2F:
+			DPRINTF("MOV B, M"); // TODO: Implement
+			break;
 
-			case 0x2E:
-				DPRINTF("ADD A, F");
-				vm->BC.hi = vm->AF.lo;
-				break;
+		case 0x38:
+			DPRINTF("MOV D,A");
+			vm->DE.hi = vm->AF.hi;
+			break;
 
-			case 0x2F:
-				DPRINTF("MOV B,M");
-				vm->BC.hi = vm->AF.lo; // TODO: IMplement
-				break;
+		case 0x39:
+			DPRINTF("MOV D,B");
+			vm->DE.hi = vm->BC.lo;
+			break;
 
-				// Adds
-			case 0x10:
-				DPRINTF("ADD A, B");
-				vm->AF.hi += vm->BC.hi;
-				break;
+		case 0x3a:
+			DPRINTF("MOV D,C");
+			vm->DE.hi = vm->DE.hi;
+			break;
 
-			case 0x11:
-				DPRINTF("ADD A, C");
-				vm->AF.hi += vm->BC.lo;
-				break;
+		case 0x3b:
+			DPRINTF("MOV D,E");
+			vm->DE.hi = vm->DE.lo;
+			break;
 
-			case 0x12:
-				DPRINTF("ADD A, D");
-				vm->AF.hi += vm->DE.hi;
-				break;
+		case 0x3c:
+			DPRINTF("MOV D,H");
+			vm->DE.hi = vm->HL.hi;
+			break;
 
-			case 0x13:
-				DPRINTF("ADD A, E");
-				vm->AF.hi += vm->DE.lo;
-				break;
+		case 0x3d:
+			DPRINTF("MOV D, L");
+			vm->DE.hi = vm->HL.lo;
+			break;
 
-			case 0x14:
-				DPRINTF("ADD A, H");
-				vm->AF.hi += vm->HL.hi;
-				break;
+		case 0x3e:
+			DPRINTF("MOV D, F");
+			vm->DE.hi = vm->AF.lo;
+			break;
 
-			case 0x15:
-				DPRINTF("ADD A, L");
-				vm->AF.hi += vm->HL.lo;
-				break;
+		case 0x3f:
+			DPRINTF("MOV D, M"); //TODO: Implement
+			break;
 
-			case 0x16:
-				DPRINTF("ADD A, F");
-				vm->AF.hi += vm->AF.lo;
-				break;
+		case 0xa0:
+			DPRINTF("MVI A, im");
+			vm->AF.hi = get_pc_8(vm);
+			break;
+
+		case 0xa1:
+			DPRINTF("MVI B, im");
+			vm->BC.hi = get_pc_8(vm);
+			break;
+
+		case 0xa2:
+			DPRINTF("MVI C, im");
+			vm->BC.lo = get_pc_8(vm);
+			break;
+
+			// Adds
+		case 0x10:
+			DPRINTF("ADD A, B");
+			vm->AF.hi += vm->BC.hi;
+			break;
+
+		case 0x11:
+			DPRINTF("ADD A, C");
+			vm->AF.hi += vm->BC.lo;
+			break;
+
+		case 0x12:
+			DPRINTF("ADD A, D");
+			vm->AF.hi += vm->DE.hi;
+			break;
+
+		case 0x13:
+			DPRINTF("ADD A, E");
+			vm->AF.hi += vm->DE.lo;
+			break;
+
+		case 0x14:
+			DPRINTF("ADD A, H");
+			vm->AF.hi += vm->HL.hi;
+			break;
+
+		case 0x15:
+			DPRINTF("ADD A, L");
+			vm->AF.hi += vm->HL.lo;
+			break;
+
+		case 0x16:
+			DPRINTF("ADD A, F");
+			vm->AF.hi += vm->AF.lo;
+			break;
 
 			// SUBS
-			case 0x30:
-				DPRINTF("SUB A, B");
-				vm->AF.hi -= vm->BC.hi;
-				if (vm -> AF.hi < vm -> BC.hi) {
-					set_flag(vm, FLAG_C); 
-				} else if (vm -> AF.hi == vm -> BC.hi) { 
-					set_flag(vm, FLAG_Z);
-				}
-				break;
-			
-			// CMPS
-			case 0x40:
-				DPRINTF("CMP A, B");
-				
-				if (vm -> AF.hi < vm -> BC.hi) {
-					// carry flag on
-					set_flag(vm, FLAG_C);
-				} else if ( vm -> AF.hi == vm -> BC.hi){
-					// zero flag on
-					set_flag(vm, FLAG_Z);
-				}
-
-				break;
-		
-			// JMPS
-			case 0xA0:
-			DPRINTF("JZ addr");
-			if (get_flag(vm, FLAG_Z)) vm->PC.val = get_pc_16(vm);
+		case 0x80:
+			DPRINTF("SUB A, B, %x, %x", vm->AF.hi, vm->BC.hi);
+			vm->AF.hi -= vm->BC.hi;
+			if (vm -> AF.hi < vm -> BC.hi) {
+				set_flag(vm, FLAG_C); 
+			} else if (vm -> AF.hi == vm -> BC.hi) { 
+				set_flag(vm, FLAG_Z);
+			}
 			break;
+
+			// CMPS
+		case 0x90:
+			DPRINTF("CMP A, B");
+
+			if (vm -> AF.hi < vm -> BC.hi) {
+				// carry flag on
+				set_flag(vm, FLAG_C);
+			} else if ( vm -> AF.hi == vm -> BC.hi){
+				// zero flag on
+				set_flag(vm, FLAG_Z);
+			}
+
+			break;
+
+			// JMPS
+		case 0xb0:
+			DPRINTF("JZ addr");
+			if (get_flag(vm, FLAG_Z)) vm-> PC.val = get_pc_16(vm);
+			break;
+
+		case 0xb1:
+			DPRINTF("JNZ addr");
+			if (!get_flag(vm, FLAG_Z)) vm-> PC.val = get_pc_16(vm);
+			break;
+			
 
 		// NOPS
 		case 0xEA:
--- main.c
+++ main.c
@@ -11,7 +11,18 @@ void execute(Machine* vm, u8 opcode);
 int main()
 {
 	// creating the memory
-	u8 stream[1024] = {0xea, 0x50,59,0x51,10, 0x10};
+	u8 stream[1024] = {
+		0x50, 0x00,      // MVI A,0
+		0x51, 0x01,      // MVI B,1
+		0x52, 0x0A,      // MVI C,10   ; loop counter = 10
+
+		// loop_start (address 6)
+		0x10,            // ADD A,B    ; A = A+B
+		0x28,		// MOV B,A
+		0x62, 		// DEC C
+		0xA1, 0x00,0x06,       // JNZ 6       ; jump to loop_start
+		0x00 // EXITS
+	};
 	Memory mem = (Memory) {
 		.stream = 0,
 	};
@@ -33,14 +44,14 @@ int main()
 			
 	// the fetch, decode and execute loop
 	
-	for (int i=0;i<9;i++) {
+	for (int i=0;i<99;i++) {
 		// fetch
 		u8 opcode = get_val_at_pc(&vm);
 
 		DPRINTF("\nTHE OPCODE FETCHED IS 0x%x", opcode);
 		execute(&vm, opcode);
 		vm.cycles += 1;
-		printf("The value at regiser A is %d\n",vm.AF.hi);
+		printf("The value at regiser A is %x and B is %x\n",vm.AF.hi, vm.BC.hi);
 	}
 
 	return 0;

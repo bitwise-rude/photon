4b28e2f54ccefa73ce8d75fac76d8353e8e1b403
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-02-26 07:26:43 +0545

    [documented] memory mappings insertion process

--- memory.c
+++ memory.c
@@ -1,5 +1,21 @@
 #include "memory.h"
 
+/* 
+ * Memory.h includes Memory Mappings to several controllers and timers, screen, etc.
+ * Memory shouldn't be accessed through other means expect the following two methods
+ * Memory mappings are (or will be) done in two functions listed below.
+ *
+ * Below are the mappings:
+ *
+ *            Memory Address             |         Mappings
+ *
+ * 	     0x0000 - 0xFFFF                     Program ROM 
+ */
+
+/* 
+ * Program ROM = Single Dimensional Array in both ESP and PC port
+ */
+
 u8 memory_read_8(Memory* mem, u16 addr){
 	 return mem->stream[addr];
 }

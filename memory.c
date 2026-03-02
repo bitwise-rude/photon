7ab3cfa639719a69564c884e9477db7cef14d11f
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-03-02 06:30:46 +0545

    [added] support for multiple memory mapped controllers

--- memory.c
+++ memory.c
@@ -1,25 +1,23 @@
 #include "memory.h"
 
-/* 
- * Memory.h includes Memory Mappings to several controllers and timers, screen, etc.
- * Memory shouldn't be accessed through other means expect the following two methods
- * Memory mappings are (or will be) done in two functions listed below.
- *
- * Below are the mappings:
- *
- *            Memory Address             |         Mappings
- *
- * 	     0x0000 - 0xFFFF                     Program ROM 
- */
+// Memory Mappings explained in `instructions` 
 
 /* 
  * Program ROM = Single Dimensional Array in both ESP and PC port
  */
 
 u8 memory_read_8(Memory* mem, u16 addr){
-	 return mem->stream[addr];
+	if (addr >= 0x0000 && addr <= 0xFEFF) return mem->stream[addr];
+	else {
+		printf("[MEMORY READ AT UNIMPLEMENTED SECTION %.04x ]", addr);
+		exit(0);
+	}
 }
 
 void memory_write_8(Memory* mem, u16 addr, u8 val){
-	 mem->stream[addr] = val;
+	if (addr >= 0x0000 && addr <= 0xFEFF) mem->stream[addr] = val;
+	else {
+		printf("[MEMORY WRITE AT UNIMPLEMENTED SECTION %.04x WITH DATA %.02x]", addr, val);
+		exit(0);
+	}
 }

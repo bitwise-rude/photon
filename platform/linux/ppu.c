a3d7d79745a24828739b3a16775d4d5348024104
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-03-12 06:32:34 +0545

    added: framebuffer is displayed by raylib part on linux

--- platform/linux/ppu.c
+++ platform/linux/ppu.c
@@ -2,9 +2,9 @@
 #include "../ppu.h"
 #include <stdio.h>
 
-#define SCREEN_HEIGHT 128
-#define SCREEN_WIDTH 64
-#define SCREEN_SCALE 5
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_SCALE 8
 
 void graphics_init(){
 	InitWindow(SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE, "03-10");
@@ -12,8 +12,20 @@ void graphics_init(){
 
 int show_buffer(u8 frame_buffer[64][128]){
 	BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("Hello World",190,200,20, LIGHTGRAY);
+
+	// Drawing the buffers
+	// Explained in instructions on how to draw
+	for(int i=0;i<SCREEN_HEIGHT;i++) {
+		for(int j=0;j<SCREEN_WIDTH;j++){
+
+			Color clr  = frame_buffer[i][j] == 0 ? BLACK: RAYWHITE;	
+
+			DrawRectangle(
+				j * SCREEN_SCALE, i * SCREEN_SCALE,
+				SCREEN_SCALE, SCREEN_SCALE, clr 
+			);
+		}
+	}
 	EndDrawing();
 
 	return WindowShouldClose();
